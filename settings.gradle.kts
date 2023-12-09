0facd9a45b77d65a6075da7da34fabcffb92e78d
reginafldaa <regina.dalimunthe@gmail.com>
2023-12-09 10:40:01 +0700

    Fetch-API

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Jobsheet6"
+include(":app")
+ 
